ede66dff950eef0548f4df56d7328a4907971d16
dizzysch <victorwilbert@gmail.com>
2023-09-29 11:29:42 -0300

    feat: add post e get de contatos

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModuloAPI.Context;
+using ModuloAPI.Entities;
+
+namespace ModuloAPI.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+            if (contato == null)
+            {
+                return NoContent();
+            }
+            return Ok(contato);
+        }
+    }
+}
