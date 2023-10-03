c533b67a637825d611108f224e4431b39b034744
dizzysch <victorwilbert@gmail.com>
2023-10-03 17:01:02 -0300

    tests: adiciona os primeiros testes

--- tests/Controllers/ContatosControllersTests.cs
+++ tests/Controllers/ContatosControllersTests.cs
@@ -1,29 +1,79 @@
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Threading.Tasks;
-using DotNet.Testcontainers.Builders;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ModuloAPI.Context;
 using ModuloAPI.Controllers;
 using ModuloAPI.Entities;
 using Moq;
+using Xunit;
 
-namespace ModuloAPI.Controllers.Tests.Controllers
+namespace ModuloAPI.Tests
 {
-    public class ContatosControllersTests
+    public class ContatoControllerTests : IDisposable
     {
-        private ContatoController contatoController;
+        private readonly AgendaContext _context;
+        private readonly ContatoController _controller;
 
-        public ContatosControllersTests()
+        public ContatoControllerTests()
         {
-            contatoController = new ContatoController(new Mock<AgendaContext>().Object);
+            var options = new DbContextOptionsBuilder<AgendaContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new AgendaContext(options);
+            _controller = new ContatoController(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        private Contato CreateSampleContato(int id, string nome)
+        {
+            return new Contato { Id = id, Nome = nome };
+        }
+
+
+        [Fact]
+        public void GetById_ReturnsOkResult_WhenContactExists()
+        {
+            // Arrange
+            _context.Add(CreateSampleContato(1, "Nome Teste"));
+            _context.SaveChanges();
+
+            // Act
+            var result = _controller.GetById(1);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
         }
 
         [Fact]
-        public void Post_Create()
+        public void GetById_ReturnsNoContent_WhenContactDoesNotExist()
         {
-            Assert.True(true);
+            // Arrange
+
+            // Act
+            var result = _controller.GetById(0);
+
+            // Assert
+            var noContentResult = Assert.IsType<NoContentResult>(result);
+            Assert.Equal(204, noContentResult.StatusCode);
         }
+
+        [Fact]
+        public void GetByName_ReturnsOkResult_WhenFindName()
+        {
+            // Arrange
+            _context.Add(CreateSampleContato(1, "Nome Teste"));
+            _context.SaveChanges();
+            // Act
+            var result = _controller.GetByName("Nome Teste");
+            // Assert           
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        // Implemente testes semelhantes para as outras ações do ContatoController (GetByName, GetAllContacts, Create, Update, Delete)
     }
 }
