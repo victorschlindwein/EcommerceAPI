4f4ee24ccead137ff3394d934bf4c38b48261116
dizzysch <victorwilbert@gmail.com>
2023-10-09 09:47:24 -0300

    fix: entidades Contatos e Endereço

--- src/Entities/Contato.cs
+++ src/Entities/Contato.cs
@@ -1,3 +1,4 @@
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -29,6 +30,11 @@ namespace ModuloAPI.Entities
             {
                 dataDeCriacao = DateTime.Now;
             }
+        }
+        public List<Endereco>? Enderecos { get; set; }
+        public Contato()
+        {
+            Enderecos = new List<Endereco>();
         }
     }
 }
---
+++ src/Migrations/20231009034343_CreatingContatoEnderecoDatabases.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModuloAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingContatoEnderecoDatabases : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnderecoId",
+                table: "Contatos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EnderecoId",
+                table: "Contatos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
